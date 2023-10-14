08fb2fe3eedf7313a7ce23ee268843748c96cd5d
Napat-Lee <frostingpotato@gmail.com>
2023-10-14 23:14:38 +0700

    No duplicate username

--- server/functions/AddUsernameS.js
+++ server/functions/AddUsernameS.js
@@ -8,6 +8,12 @@ const createName = function (data) {
   } else {
     name = data;
   }
+  let i = (global.namelist).findIndex(user => user.name == name)
+  while (i != -1) {
+    var x = (Math.floor(Math.random() * 900)+100)   //assign random ID
+    name = "KunToi#"+x;
+    i = (global.namelist).findIndex(user => user.name == name)
+  }
   let info = {'socketID':socket.id, 'name':name, 'score':0};
   let index = (global.namelist).findIndex(user => user.socketID == socket.id)   //find index of disconnected id
   //console.log(index)
