e3049a63506df41b8f16de44d3e10b07d30c7bee
Napat-Lee <frostingpotato@gmail.com>
2023-11-02 00:54:14 +0700

    Select Gamemode
    +move all gamemode stuff to gamemode.js

--- server/functions/RandomWordS.js
+++ server/functions/RandomWordS.js
@@ -1,43 +1,12 @@
 const { wordlist } = require('../utils/wordlist.js');
 const { io } = require('../utils/socket-server.js')
 const { getNamelist, getUserInfo, getSpecificInfo, addNamelist, removeNamelist, findNameIndex, updateUserInfo, addRoomlist, removeRoomlist, updateRoomlist, getRoomlist } = require('../utils/serverdata.js');
-
-var len;
-var word;
-
-const easyMode = function (rnd) {   //assign prob. for easy mode
-  if (rnd < 0.1) {return(3)}
-  else if (rnd >= 0.1 && rnd < 0.3) {return(4)}
-  else if (rnd >= 0.3 && rnd < 0.7) {return(5)}
-  else if (rnd >= 0.7 && rnd < 0.9) {return(6)}
-  else {return(7)};
-}
-
-const mediumMode = function (rnd) {   //assign prob. for medium mode
-  if (rnd < 0.1) {return(6)}
-  else if (rnd >= 0.1 && rnd < 0.3) {return(7)}
-  else if (rnd >= 0.3 && rnd < 0.7) {return(8)}
-  else if (rnd >= 0.7 && rnd < 0.9) {return(9)}
-  else {return(10)};
-}
-
-const hardMode = function (rnd) {   //assign prob. for hard mode
-  if (rnd < 0.1) {return(9)}
-  else if (rnd >= 0.1 && rnd < 0.3) {return(10)}
-  else if (rnd >= 0.3 && rnd < 0.7) {return(11)}
-  else if (rnd >= 0.7 && rnd < 0.9) {return(12)}
-  else {return(13)};
-}
+const { selectMode } = require('../utils/gamemode.js')
 
 const randomLength = function (mode) {    //get random length of word
   var rnd = Math.random();
-  if (mode == 2) {
-    len = mediumMode(rnd);
-  } else if (mode == 3) {
-    len = hardMode(rnd);
-  } else {
-      len = easyMode(rnd);
-  }
+  const gm = selectMode(mode);
+  let len = gm(rnd);
   //console.log(rnd ,len);
   return len;
 };
@@ -45,9 +14,9 @@ const randomLength = function (mode) {    //get random length of word
 const randomWord = function (mode) {     //assign random word to each player
   const socket = this;
   let len = randomLength(mode)
-  var size = Object.values(wordlist)[len-2].length;
-  var pos = Math.floor(Math.random()*size);
-  word = wordlist[len][pos];
+  let size = Object.values(wordlist)[len-2].length;
+  let pos = Math.floor(Math.random()*size);
+  let word = wordlist[len][pos];
   let index = findNameIndex(socket.id,"id");
   let room = getSpecificInfo(index,"room");
   io.in(room).emit("send_word", {len,word})
--- server/utils/gamemode.js
+++ server/utils/gamemode.js
@@ -2,4 +2,40 @@ const baseAdd = [0,0,20,30,40,60,80,100,120,150,180,210,240,270,300,350]
 const baseSubtract = [0,0,30,40,50,60,70,80,90,100,110,120,130,140,150,160]
 const multiplier = [0,0,2,3,6,8,12,16,20,24,30,36,46,60,70]
 
-module.exports = { baseAdd, baseSubtract, multiplier }
+const easyMode = function (rnd) {   //assign prob. for easy mode
+  if (rnd < 0.1) {return(3)}
+  else if (rnd >= 0.1 && rnd < 0.3) {return(4)}
+  else if (rnd >= 0.3 && rnd < 0.7) {return(5)}
+  else if (rnd >= 0.7 && rnd < 0.9) {return(6)}
+  else {return(7)};
+}
+
+const mediumMode = function (rnd) {   //assign prob. for medium mode
+  if (rnd < 0.1) {return(6)}
+  else if (rnd >= 0.1 && rnd < 0.3) {return(7)}
+  else if (rnd >= 0.3 && rnd < 0.7) {return(8)}
+  else if (rnd >= 0.7 && rnd < 0.9) {return(9)}
+  else {return(10)};
+}
+
+const hardMode = function (rnd) {   //assign prob. for hard mode
+  if (rnd < 0.1) {return(9)}
+  else if (rnd >= 0.1 && rnd < 0.3) {return(10)}
+  else if (rnd >= 0.3 && rnd < 0.7) {return(11)}
+  else if (rnd >= 0.7 && rnd < 0.9) {return(12)}
+  else {return(13)};
+}
+
+const selectMode = function (mode) {
+  if (mode == 1) {
+    return easyMode;
+  } else if (mode == 2) {
+    return mediumMode;
+  } else if (mode == 3) {
+    return hardMode;
+  } else {
+    return easyMode;
+  }
+
+}
+module.exports = { baseAdd, baseSubtract, multiplier, selectMode }
