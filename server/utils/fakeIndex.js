e3735113fcdaf21392c4ce6e43f1ff56ab28a4d6
Napat-Lee <frostingpotato@gmail.com>
2023-11-03 14:10:29 +0700

    need help with index.html

--- server/utils/fakeIndex.js
+++ server/utils/fakeIndex.js
@@ -1,1 +1,14 @@
-let things = 'trytry';
+import io from './socket-server'
+import count from './serverdata.js'
+
+let playerNo = count;
+document.getElementById("count").innerHTML = playerNo;
+
+function nukeServer () {
+  //io.disconnectSockets();
+  alert("hi!")
+}
+
+
+
+
