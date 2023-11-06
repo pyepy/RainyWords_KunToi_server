3b3967a449d5434c2b44dc978084779f071b0a58
Napat-Lee <frostingpotato@gmail.com>
2023-11-06 18:48:17 +0700

    Prep for Special Score

--- server/utils/gamemode.js
+++ server/utils/gamemode.js
@@ -1,6 +1,7 @@
 const baseAdd = [0,0,20,30,40,60,80,100,120,150,180,210,240,270,300,350]
 const baseSubtract = [0,0,30,40,50,60,70,80,90,100,110,120,130,140,150,160]
 const multiplier = [0,0,2,3,6,8,12,16,20,24,30,36,46,60,70]
+const nWord = [300,600,1200]
 
 const easyMode = function (rnd) {   //assign prob. for easy mode
   if (rnd < 0.1) {return(3)}
