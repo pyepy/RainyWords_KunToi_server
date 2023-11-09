0042f72a41b74f9501cda923e4969649491df2fb
pyepy <123175407+pyepy@users.noreply.github.com>
2023-11-09 21:39:08 +0700

    fix blind

--- server/functions/PowerUp.js
+++ server/functions/PowerUp.js
@@ -8,7 +8,7 @@ const blindEnemy = function () {
   const socket = this;
   let index = findNameIndex(socket.id,"id");
   let room = getSpecificInfo(index,"room");
-  socket.in(room).emit("blind_powerup_activated");
+  socket.in(room).emit("blind_enemy");
 }
 
 module.exports = { blindEnemy }
